0089c667fa6c91bf308eeb61ac0442ebe80f0166
Logan Koester <logan@logankoester.com>
2015-12-09 09:19:25 -0500

    Bump 0.1.0

--- metadata.rb
+++ metadata.rb
@@ -4,4 +4,4 @@ maintainer_email 'logan@logankoester.com'
 license          'MIT'
 description      'MongoDB database server'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.0.0'
+version          '0.1.0'
