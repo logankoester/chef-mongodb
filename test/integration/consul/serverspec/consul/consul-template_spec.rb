937536a45cd6b1afaef2170bf86d8925c55e3fad
Logan Koester <logan@logankoester.com>
2015-12-15 07:03:17 -0500

    Fixes test that would fail if a consul server is not present

--- test/integration/consul/serverspec/consul/consul-template_spec.rb
+++ test/integration/consul/serverspec/consul/consul-template_spec.rb
@@ -1,17 +1,5 @@
 require 'spec_helper'
 
-describe package('mongodb') do
-  it { should be_installed }
-end
-
-describe package('mongodb-tools') do
-  it { should be_installed }
-end
-
-describe command('supervisorctl status mongodb') do
-  its(:stdout){ should match(/RUNNING/) }
-end
-
 describe file('/etc/mongodb.conf') do
   it { should be_file }
   it { should be_mode 644 }
